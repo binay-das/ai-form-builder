5e6d6192646914ce9112cf8c8940ab879bf27118
Binay <binaydas1123@gmail.com>
2026-05-03 11:05:46 +0530

    feat: auth setup using next auth

---
+++ lib/auth.ts
@@ -0,0 +1,66 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials"
+import { prisma } from "./prisma";
+import bcrypt from "bcryptjs";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: { label: "Email", type: "email", placeholder: "Enter your email" },
+                password: { label: "Password", type: "password", placeholder: "Enter your password" }
+            },
+            async authorize(credentials, req) {
+                const email = credentials?.email;
+                const password = credentials?.password;
+
+                if (!email || !password) {
+                    return null;
+                }
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email
+                    },
+                });
+
+                if (!user || !user.password) {
+                    return null;
+                }
+
+                const isPasswordValid = await bcrypt.compare(password, user.password);
+
+                if (!isPasswordValid) {
+                    return null;
+                }
+
+
+                return {
+                    id: user.id,
+                    email: user.email,
+                    name: user.name
+                };
+            }
+        })
+    ],
+    session: {
+        strategy: "jwt"
+    },
+    callbacks: {
+        async session({ session, token }) {
+            session.user.id = token.id;
+            return session;
+        },
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        }
+    },
+    pages: {
+        signIn: "/signin",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+}
